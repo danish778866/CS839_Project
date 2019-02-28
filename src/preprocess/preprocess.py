3438ea16ad605ce379ddcdb2b1375d884f3fa9e9
Mohammed Danish Shaikh <danish778866@gmail.com>
2019-02-27 19:44:22 -0600

    Add preprocessing

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -13,11 +13,67 @@ def preprocess_labeled_files(path, words):
         words.extend(current_line.split())
     file.close()
 
-def generate_candidates_labels(words):
+def is_negative(word):
+    ret_val = False
+    if word[0].islower():
+        ret_val = True
+    elif word[0].isdigit():
+        ret_val = True
+    return ret_val
+
+def generate_candidates_labels(words, threshold, candidates, labels):
     num_words = len(words)
+    gen_threshold = threshold
     for i in range(num_words):
-        print(words[i])
+        if i + threshold > num_words - 1:
+            gen_threshold = num_words - i
+        prune = False
+        current_word = words[i]
+        is_person = False
+        end_person = False
+        if is_negative(current_word):
+            prune = True
+        else:
+            if current_word.startswith("<person>"):
+                current_word = current_word.replace("<person>", "")
+                is_person = True
+                labels.append(1)
+            elif current_word.endswith("</person>"):
+                current_word = current_word.replace("</person>", "")
+                labels.append(1)
+            else:
+                labels.append(0)
+            candidates.append(current_word)
+            
+        j = 1
+        #print("Going for..." + words[i])
+        while (j < gen_threshold) and (not prune):
+            if is_negative(words[i + j]):
+                prune = True
+            else:
+                current_word = current_word + " " + words[i + j]
+                if "<person>" in current_word:
+                    current_word = current_word.replace("<person>", "")
+                elif "</person>" in current_word:
+                    current_word = current_word.replace("</person>", "")
+                    end_person = True
+                if is_person:
+                    labels.append(1)
+                    if end_person:
+                        is_person = False
+                else:
+                    labels.append(0)
+                candidates.append(current_word)
+                j = j + 1
+
 
 words = []
+candidates = []
+labels = []
 preprocess_labeled_files("/home/mohdanishaikh/Projects/CS839_Project/data/511.txt", words)
-generate_candidates_labels(words)
+generate_candidates_labels(words, 4, candidates, labels)
+num_candidates = len(candidates)
+for i in range(num_candidates):
+    print(candidates[i] + "-" + str(labels[i]))
+print(candidates)
+print(labels)
