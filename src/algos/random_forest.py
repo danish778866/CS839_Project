e77c317150f6df7a4d6a7830fdbaa69d1a8f80fa
Mohammed Danish Shaikh <danish778866@gmail.com>
2019-03-05 11:25:03 -0600

    Random forest algorithm

--- src/algos/random_forest.py
+++ src/algos/random_forest.py
@@ -1,31 +1,27 @@
 #Reference : https://stackabuse.com/random-forest-algorithm-with-python-and-scikit-learn/
 import pandas as pd
 import numpy as np
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-dataset = pd.read_csv("/Users/somya/Desktop/sem2/CS839_Project/data/features/features.csv")
-labels = pd.read_csv("/Users/somya/Desktop/sem2/CS839_Project/data/labels/all_labels.csv")
-
-X = dataset.values
+project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+features_csv = project_dir + os.sep + "data" + os.sep + "features" + os.sep + "features.csv" 
+labels_csv = project_dir + os.sep + "data" + os.sep + "labels" + os.sep + "labels.csv"
+feature_vector = pd.read_csv(features_csv)
+labels = pd.read_csv(labels_csv)
+x = feature_vector.values
 y = labels.values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-sc = StandardScaler()  
-X_train = sc.fit_transform(X_train)  
-X_test = sc.transform(X_test)  
-regressor = RandomForestRegressor(n_estimators=20, random_state=0)  
-regressor.fit(X_train, y_train.ravel())
-y_pred = regressor.predict(X_test)
-thresh = []
-for yi in y_pred:
-    if yi<0.5:
-        thresh.append(0)
-    else:
-        thresh.append(1)
-
-print(confusion_matrix(y_test,thresh))
-print(classification_report(y_test,thresh))
-print(accuracy_score(y_test, thresh))
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
+classifier = RandomForestClassifier(n_estimators=20, random_state=0)  
+classifier.fit(x_train, y_train.ravel())
+y_pred = classifier.predict(x_test)
+print("...Confusion Matrix...")
+print(confusion_matrix(y_test,y_pred))
+print("...Classification Report...")
+print(classification_report(y_test,y_pred))
+print("...Accuracy Score...")
+print(accuracy_score(y_test, y_pred))
 
