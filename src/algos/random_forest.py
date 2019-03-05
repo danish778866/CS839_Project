f777c23a4a6d17b4bb489a2225dd58f06f20cec7
srora <somyarora96@gmail.com>
2019-03-05 02:41:36 -0600

    run random forest

--- src/algos/random_forest.py
+++ src/algos/random_forest.py
@@ -6,17 +6,26 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
 
-dataset = pd.read_csv("D:/Datasets/bill_authentication.csv")
-X = dataset.iloc[:, 0:4].values  
-y = dataset.iloc[:, 4].values
+dataset = pd.read_csv("/Users/somya/Desktop/sem2/CS839_Project/data/features/features.csv")
+labels = pd.read_csv("/Users/somya/Desktop/sem2/CS839_Project/data/labels/all_labels.csv")
+
+X = dataset.values
+y = labels.values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 sc = StandardScaler()  
 X_train = sc.fit_transform(X_train)  
 X_test = sc.transform(X_test)  
 regressor = RandomForestRegressor(n_estimators=20, random_state=0)  
-regressor.fit(X_train, y_train)  
-y_pred = regressor.predict(X_test)  
-print(confusion_matrix(y_test,y_pred))  
-print(classification_report(y_test,y_pred))  
-print(accuracy_score(y_test, y_pred))  
+regressor.fit(X_train, y_train.ravel())
+y_pred = regressor.predict(X_test)
+thresh = []
+for yi in y_pred:
+    if yi<0.5:
+        thresh.append(0)
+    else:
+        thresh.append(1)
+
+print(confusion_matrix(y_test,thresh))
+print(classification_report(y_test,thresh))
+print(accuracy_score(y_test, thresh))
 
