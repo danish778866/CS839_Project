e36a0b891e550d4bec46e12f3bf403dd3ce1e93a
Mohammed Danish Shaikh <danish778866@gmail.com>
2019-03-06 00:38:16 -0600

    Generate data

--- src/preprocess/preprocess.py
+++ src/preprocess/preprocess.py
@@ -39,26 +39,61 @@ def is_negative(word):
     return ret_val
 
 def clean_word(word):
-    word = word.replace("\'s", "")
-    word = re.sub(r'[^\w\s<>/]', '', word)
-    return word
+    ends_apostrophe = False
+    if word.endswith("\'s"):
+        word = word.replace("\'s ", " ")
+        ends_apostrophe = True
+    elif word.endswith("s\'"):
+        word = word.replace("s\' ", " ")
+        ends_apostrophe = True
+    word = re.sub(r'[^\w\s<>/\']', '', word)
+    return word, ends_apostrophe
 
-def get_prefix_suffix(words, index, num_words):
-    current_prefix = "N/A"
-    current_suffix = "N/A"
-    if index == 1:
-        current_prefix = words[0]
-    elif index > 1:
-        current_prefix = words[index - 2] + " " + words[index - 1]
-    if index == num_words - 2:
-        current_suffix = words[num_words - 1]
-    elif index < num_words - 2:
-        current_suffix = words[index + 1] + " " + words[index + 2]
+def get_prefix(words, index, num_words):
+    current_prefix = ""
+    if index > 0:
+        index = index - 1
+        done = False
+        num_prefixes = 0
+        while not done:
+            if words[index] == "," or words[index] == "\"":
+                index = index - 1
+            elif words[index] == ".":
+                done = True
+            else:
+                current_prefix = words[index] + " " + current_prefix
+                index = index - 1
+                num_prefixes = num_prefixes + 1
+            if num_prefixes == 2 or index < 0:
+                done = True
     current_prefix = current_prefix.replace("<person>", "").replace("</person>", "")
-    current_suffix = current_suffix.replace("<person>", "").replace("</person>", "")
     current_prefix = re.sub(r'[^\w\s]', '', current_prefix)
+    if not current_prefix:
+        current_prefix = "NA"
+    return current_prefix.strip()
+
+def get_suffix(words, index, num_words):
+    current_suffix = ""
+    if index < num_words - 1:
+        index = index + 1
+        done = False
+        num_suffixes = 0
+        while not done:
+            if words[index] == "," or words[index] == "\"":
+                index = index + 1
+            elif words[index] == ".":
+                done = True
+            else:
+                current_suffix = current_suffix + " " + words[index]
+                index = index + 1
+                num_suffixes = num_suffixes + 1
+            if num_suffixes == 2 or index > num_words - 1:
+                done = True
+    current_suffix = current_suffix.replace("<person>", "").replace("</person>", "")
     current_suffix = re.sub(r'[^\w\s]', '', current_suffix)
-    return current_prefix.strip(), current_suffix.strip()
+    if not current_suffix:
+        current_suffix = "NA"
+    return current_suffix.strip()
 
 def generate_candidates_labels(words, threshold):
     num_words = len(words)
@@ -68,12 +103,14 @@ def generate_candidates_labels(words, threshold):
     labels = []
     prefix = []
     suffix = []
+    feature_ends_apostrophe = []
     for i in range(num_words):
         if i + threshold > num_words - 1:
             gen_threshold = num_words - i
         prune = False
-        current_word = clean_word(words[i])
-        current_prefix, current_suffix = get_prefix_suffix(words, i, num_words)
+        current_word, ends_apostrophe = clean_word(words[i])
+        current_prefix = get_prefix(words, i, num_words)
+        current_suffix = get_suffix(words, i, num_words)
         end_person = False
         not_title = True
         if current_word in ["Sir", "Mr", "Ms", "Dr", "Prof", "St"]:
@@ -99,6 +136,10 @@ def generate_candidates_labels(words, threshold):
                     labels.append(0)
             if not_title:
                 candidates.append(current_word)
+                if ends_apostrophe:
+                    feature_ends_apostrophe.append(1)
+                else:
+                    feature_ends_apostrophe.append(0)
                 prefix.append(current_prefix)
                 suffix.append(current_suffix)
             
@@ -109,8 +150,9 @@ def generate_candidates_labels(words, threshold):
             if is_negative(words[i + j]):
                 prune = True
             else:
-                current_prefix, current_suffix = get_prefix_suffix(words, i + j, num_words)
-                current_word = current_word + " " + clean_word(words[i + j])
+                current_suffix = get_suffix(words, i + j, num_words)
+                current_local_word, ends_apostrophe = clean_word(words[i + j])
+                current_word = current_word + " " + current_local_word
                 if "<person>" in current_word and "</person>" in current_word:
                     current_word = current_word.replace("<person>", "").replace("</person>", "")
                 elif "<person>" in current_word:
@@ -125,10 +167,14 @@ def generate_candidates_labels(words, threshold):
                 else:
                     labels.append(0)
                 candidates.append(current_word)
+                if ends_apostrophe:
+                    feature_ends_apostrophe.append(1)
+                else:
+                    feature_ends_apostrophe.append(0)
                 prefix.append(current_prefix)
                 suffix.append(current_suffix)
                 j = j + 1
-    return candidates, labels, prefix, suffix
+    return candidates, labels, prefix, suffix, feature_ends_apostrophe
 
 def write_data(path, content):
     f = open(path, "w")
@@ -136,8 +182,8 @@ def write_data(path, content):
         f.write(str(line) + "\n")
     f.close()
 
-def preprocess_driver(data_dir, candidates_dir, labels_dir):
-    files = glob.glob(data_dir)
+def preprocess_driver(data_files, candidates_dir, labels_dir, features_dir):
+    files = glob.glob(data_files)
     words = []
     for name in files:
         words = preprocess_labeled_files(name)  
@@ -146,24 +192,19 @@ def preprocess_driver(data_dir, candidates_dir, labels_dir):
         prefix_file = candidates_dir + os.sep + file_name + ".pre"
         suffix_file = candidates_dir + os.sep + file_name + ".suf"
         labels_file = labels_dir + os.sep + file_name 
-        candidates, labels, prefix, suffix = generate_candidates_labels(words, 4)
+        feature_ends_apostrophe_file = features_dir + os.sep + "ends_apostrophe.csv"
+        candidates, labels, prefix, suffix, feature_ends_apostrophe = generate_candidates_labels(words, 4)
         write_data(candidates_file, candidates)
         write_data(labels_file, labels)
         write_data(prefix_file, prefix)
         write_data(suffix_file, suffix)
+        write_data(feature_ends_apostrophe_file, feature_ends_apostrophe)
 
 
 
-#words = []
-#candidates = []
-#labels = []
-path = "/home/mohdanishaikh/Projects/CS839_Project/data/*.txt"
-candidates_dir = "/home/mohdanishaikh/Projects/CS839_Project/data/candidates"
-labels_dir = "/home/mohdanishaikh/Projects/CS839_Project/data/labels"
-preprocess_driver(path, candidates_dir, labels_dir)
-#preprocess_labeled_files(path, words)
-#generate_candidates_labels(words, 4, candidates, labels)
-#num_candidates = len(candidates)
-#print(words)
-#write_data(output_path, "candidates.txt", candidates)
-#write_data(output_path, "labels.txt", labels)
+project_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+data_files = project_dir + os.sep + "data" + os.sep + "*.txt"
+candidates_dir = project_dir + os.sep + "data" + os.sep + "candidates"
+labels_dir = project_dir + os.sep + "data" + os.sep + "labels"
+features_dir = project_dir + os.sep + "data" + os.sep + "features"
+preprocess_driver(data_files, candidates_dir, labels_dir, features_dir)
